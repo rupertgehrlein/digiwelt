4bdfda8b19d517505de483a0f2764211060202e2
Otto Gaudl <s9579354@stud.uni-frankfurt.de>
2025-02-20 13:19:21 +0100

    makes tools a working site

--- src/app/components/tools/tools.component.ts
+++ src/app/components/tools/tools.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class ToolsComponent {
 
+  CompUberTools: any[] = [{heading:"CompÜber", description:"Beschreibung 1", link: "https://www.youtube.com/"}];
+  InfoDatenTools: any[] = [{heading:"InfoDaten", description:"Beschreibung 1", link: "https://www.youtube.com/"}];
+  GrundProgTools: any[] = [{heading:"GrundProg", description:"Beschreibung 1", link: "https://www.youtube.com/"}];
+  VernetzungTools: any[] = [{heading:"Vernetzung", description:"Beschreibung 1", link: "https://www.youtube.com/"}];
+  AllgemeinTools: any[] = [{heading:"Allgemein", description:"Beschreibung 1", link: "https://www.youtube.com/"}];
+
 }
